4dbbd1a76d2a12ed6986603d00ff80701eb0f673
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-07-14 19:40:25 +0530

    Time: 21 ms (60.59%) | Memory: 25.1 MB (89.60%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,17 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res=[1]*(len(nums))
+        prefix=1
+        for i in range(len(nums)):
+            res[i]=prefix
+            prefix=prefix*nums[i]
+        
+        post_fix=1
+
+        for i in range(len(nums)-1,-1,-1):
+            res[i]=res[i]*post_fix
+            post_fix=post_fix*nums[i]
+
+        return res
+
+            
