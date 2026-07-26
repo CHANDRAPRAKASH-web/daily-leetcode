37c10621e77738495f079d614936bc1f4828c609
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-07-26 20:14:44 +0530

    Time: 15 ms (26.94%) | Memory: 20.7 MB (99.91%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -4,17 +4,12 @@ class Solution:
         max_right=[0]*(len(height))
         ml=0
         mr=0
-
         for i in range(1,len(height)):
             ml=max(ml,height[i-1])
             max_left[i]=ml
-
-
         for i in range(len(height)-2,-1,-1):
             mr=max(mr,height[i+1])
             max_right[i]=mr
-
-        
 
         s=0
 
