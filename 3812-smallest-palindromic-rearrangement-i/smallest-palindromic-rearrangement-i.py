2080cf3ccc58de2aca01cd207b5511d6b195293e
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-07-28 23:03:14 +0530

    Time: 194 ms (66.36%) | Memory: 20.9 MB (65.44%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -1,13 +1,20 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
        
-        p=len(s)//2
-        base=sorted(s[:p])
-        if len(s)%2==1:
-            a=[s[p]]
-        else:
-            a=[]
-        reverse_base=base[::-1]
+       p=len(s)//2
+       bucket=[0]*26
+       left=""
 
-        return "".join(base+a+reverse_base)
-        
+       for i in range(p):
+        bucket[ord(s[i])-ord('a')]+=1
+
+       for i in range(26):
+        if bucket[i]>0:
+            left+=chr(i+ord('a'))*bucket[i]
+
+
+       mid = s[p] if len(s)%2==1 else ""
+
+       right=left[::-1]
+       
+       return left+mid+right
