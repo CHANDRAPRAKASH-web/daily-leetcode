74b7452314e138d07d94682ff35ecf5614092f9a
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-07-30 18:30:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (83.33%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -1,17 +1,4 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        count=0
-        current_cost=1
-        total_cost=0
-        for i in range(len(word)):
-            if count==8:
-                count=0
-                current_cost+=1
-
-            total_cost+=current_cost
-            count+=1
-
-        return total_cost
-            
-
-        
+       n=len(word)
+       return sum(i//8 + 1 for i in range(n))
