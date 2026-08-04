9f6d2fee326a2151d8ffa5254b73e9e1db8290a8
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-08-04 10:12:16 +0530

    Time: 405 ms (9.31%) | Memory: 136.8 MB (19.92%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,3 +1,19 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-       return True
+        memo={}
+
+        def dp(l,r):
+            if l==r:
+                return piles[l]
+
+            if (l,r) in memo:
+                return memo[(l,r)]
+
+            left=piles[l]-dp(l+1,r)
+            right=piles[r]-dp(l,r-1)
+            memo[(l,r)]=max(left,right)
+
+            return max(left,right)
+
+        return dp(0,len(piles)-1)>0
+       
