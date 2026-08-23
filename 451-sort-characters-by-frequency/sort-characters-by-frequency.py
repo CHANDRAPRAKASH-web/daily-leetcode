81c1b11767ac40d058c094008dfa0692f2ea2c9d
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-08-23 23:19:21 +0530

    Time: 34 ms (8.23%) | Memory: 28.4 MB (8.38%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -4,14 +4,14 @@ class Solution:
         freq=[[] for _ in range(len(s)+1)]
         a={}
         for i in s:
-            a[i]=1+a.get(i,0)
+            a[i]=1+a.get(i,0) # t:1 r:1 e:2
         
         for key,value in a.items():
-            freq[value].append(key)
+            freq[value].append(key) # 1:t,r 2:e 
 
-        for i in range(len(freq)-1,-1,-1):
+        for i in range(len(freq)-1,0,-1):
             for j in freq[i]:
-                res.append(j*i)
+                res.append(j*i) # [e,e,t,r] * s="" s+=j*i
 
         return "".join(res)
 
