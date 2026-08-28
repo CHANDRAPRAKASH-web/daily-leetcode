44c33b213c0c78c7026b3bdf8b4b19effa597a93
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-08-28 19:37:37 +0530

    Time: 24 ms (32.03%) | Memory: 30.9 MB (41.13%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -5,13 +5,10 @@ class Solution:
         for i in range(k):
             left_sum+=nums[i]
         max_sum=left_sum
-        n=len(nums)
-        a=k-1
-        for i in range(len(nums)-1,len(nums)-k-1,-1):
-            right_sum+=nums[i]
-            left_sum-=nums[a]
-            max_sum=max(max_sum,right_sum+left_sum)
-            a-=1
+        for i in range(1,k+1):
+            left_sum-=nums[k-i]
+            right_sum+=nums[-i]
+            max_sum=max(max_sum,left_sum+right_sum)
         return max_sum
 
         
