c6bed023ed5a3db51fd3051ae6cc3556ee73ef10
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-09-03 19:25:33 +0530

    Time: 21 ms (76.22%) | Memory: 35.3 MB (51.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -1,14 +1,3 @@
 class Solution:
     def uniformArray(self, nums: list[int]) -> bool:
-        smallest_odd=float('inf')
-        for i in nums:
-            if i%2==1:
-                smallest_odd=min(smallest_odd,i)
-        if smallest_odd==float('inf'):
-            return True
-        for i in nums:
-            if i%2==0 and i<smallest_odd:
-                return False
-        return True
-                
-       
+        return all(num%2==1 for num in nums) or all(num%2==0 for num in nums) or min(nums)%2==1
