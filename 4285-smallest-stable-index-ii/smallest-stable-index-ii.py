a60039e73ed52df5110c40e8d05a254b31c3262f
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-09-05 12:39:38 +0530

    Time: 148 ms (79.09%) | Memory: 33 MB (49.55%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+
+        n=len(nums)
+        mx=float('-inf')
+        suffix=[0]*n
+        suffix[n-1]=nums[-1]
+        for i in range(n-2,-1,-1):
+            suffix[i]=min(suffix[i+1],nums[i])
+        for i in range(n):
+            mx=max(mx,nums[i])
+            if mx-suffix[i]<=k:
+                return i
+        return -1
+
+        
+        
